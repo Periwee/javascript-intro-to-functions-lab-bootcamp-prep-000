5f19f6a84f26daec947f2c276633e207be6d5e4d
Esperanza Rodriguez <esperanza.rguezz@gmail.com>
2019-03-05 03:40:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string){
 
 function sayHiToGrandma(string)
 {
-  if(string.lowercase())
+  var lowercase = string.toLowerCase()
+  if(string===lowercase){
     return "I can\'t hear you!"
+  }
 }
